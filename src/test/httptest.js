8f7c33135eb57c85427e9fedf7cdcdadbf77d61b
makaikai <makaikai@accesscorporate.com.cn>
2022-07-26 21:41:29 +0800

    add test

--- src/test/httptest.js
+++ src/test/httptest.js
@@ -1,5 +1,22 @@
 const http = require("http")
+const cheerio = require('cheerio');
+
 http.request('http://www.baidu.com', (response) => {
     // console.log(response)
-    console.log(response.headers)
+
+    let data = '';
+    let i = 0;
+    response.on('data' , (chuck ) => {
+        data = data + chuck;
+        i++;
+        console.log(i)
+    })
+
+    response.on('close',() =>{
+        console.log('get all data')
+        const $ = cheerio.load(data);
+        console.log($('#su').attr('type'))
+        console.log($('#su').next().html())
+        // console.log(data)
+    })
 }).end()
