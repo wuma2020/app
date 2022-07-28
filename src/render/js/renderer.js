4f17fd804602d21f6c0599f48c4dc01687102fca
makaikai <makaikai@accesscorporate.com.cn>
2022-07-28 23:03:27 +0800

    分段

--- src/render/js/renderer.js
+++ src/render/js/renderer.js
@@ -1,4 +1,28 @@
 
+//===================初始化html===================
+const links = document.querySelectorAll('link[rel="import"]')
+
+// Import and add each page to the DOM
+Array.prototype.forEach.call(links, (link) => {
+
+    console.log(link)
+    let template = link.import.querySelector('.template')
+
+    console.log(template)
+
+    let clone = document.importNode(template.content, true)
+
+    if (link.href.match('head.html')) {
+        document.querySelector('head').appendChild(clone)
+    }
+
+    if (link.href.match('body.html')) {
+        document.querySelector('body').appendChild(clone)
+    }
+})
+
+
+
 const NOTIFICATION_TITLE = '网络变化通知'
 const NOTIFICATION_BODY = 'Notification from the Renderer process. Click to log to console.'
 const CLICK_MESSAGE = 'Notification clicked!'
@@ -7,10 +31,10 @@ const updateOnlineStatus = () => {
     document.getElementById('status').innerHTML = navigator.onLine ? 'online' : 'offline'
 
     new Notification(NOTIFICATION_TITLE, { body: navigator.onLine ? '上线' : '下线' })
-    .onclick = () => document.getElementById("output").innerText = CLICK_MESSAGE
-  
+        .onclick = () => document.getElementById("output").innerText = CLICK_MESSAGE
+
 }
-  
+
 window.addEventListener('online', updateOnlineStatus)
 window.addEventListener('offline', updateOnlineStatus)
 
@@ -21,13 +45,10 @@ updateOnlineStatus()
 const screenshot = document.getElementById('screen-shot')
 const screenshotMsg = document.getElementById('screenshot-path')
 
-screenshot.addEventListener('click',async ()=>{
+screenshot.addEventListener('click', async () => {
     var filePath = await window.electrona.mykk()
-    screenshotMsg.innerHTML = filePath;
-    console.log(filePath)
+    // screenshotMsg.innerHTML = filePath;
+    // console.log(filePath)
 })
-
-//===================主题 daisy ui===================
-
 
 
