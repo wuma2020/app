447f2c012fea633b1c444874a22f1f0d364f1550
makaikai <makaikai@accesscorporate.com.cn>
2022-07-30 12:25:07 +0800

    test

--- src/render/js/renderer.js
+++ src/render/js/renderer.js
@@ -1,54 +1,50 @@
 
 //===================初始化html===================
-const links = document.querySelectorAll('link[rel="import"]')
+var initHtml = () => {
 
-// Import and add each page to the DOM
-Array.prototype.forEach.call(links, (link) => {
+  const updateOnlineStatus = () => {
+    const NOTIFICATION_TITLE = '网络变化通知'
+    const NOTIFICATION_BODY = 'Notification from the Renderer process. Click to log to console.'
+    const CLICK_MESSAGE = 'Notification clicked!'
 
-  console.log(link)
-//   let template = link.import.querySelector('.template')
-
-//   console.log(template)
-
-//   let clone = document.importNode(template.content, true)
-
-  if (link.href.match('head.html')) {
-    //这个方法可以加载一下
-    $('#head').load('../static/head.html')
-    // document.querySelector('head').appendChild(clone)
-  }
-
-//   if (link.href.match('body.html')) {
-//     document.querySelector('body').appendChild(clone)
-//   }
-})
-
-const NOTIFICATION_TITLE = '网络变化通知'
-const NOTIFICATION_BODY = 'Notification from the Renderer process. Click to log to console.'
-const CLICK_MESSAGE = 'Notification clicked!'
-
-const updateOnlineStatus = () => {
-    document.getElementById('status').innerHTML = navigator.onLine ? 'online' : 'offline'
+    $('#status').html(navigator.onLine ? 'online' : 'offline')
 
     new Notification(NOTIFICATION_TITLE, { body: navigator.onLine ? '上线' : '下线' })
-        .onclick = () => document.getElementById("output").innerText = CLICK_MESSAGE
+      .onclick = () => $("#output").text(CLICK_MESSAGE)
+
+  }
+
+  window.addEventListener('online', updateOnlineStatus)
+  window.addEventListener('offline', updateOnlineStatus)
+
+  updateOnlineStatus()
+
+
+  //===================截图===================
+  var screenshot = document.getElementById('screen-shot')
+  const screenshotMsg = document.getElementById('screenshot-path')
+
+  // screenshot.onclick(async () => {
+  //   var filePath = await window.electrona.mykk()
+  // }
+  // )
+
+  $("#screen-shot").click(async () => {
+    var filePath = await window.electrona.mykk()
+  })
+
 
 }
 
-window.addEventListener('online', updateOnlineStatus)
-window.addEventListener('offline', updateOnlineStatus)
-
-updateOnlineStatus()
+$(initHtml())
 
 
-//===================截图===================
-const screenshot = document.getElementById('screen-shot')
-const screenshotMsg = document.getElementById('screenshot-path')
 
-screenshot.addEventListener('click', async () => {
-    var filePath = await window.electrona.mykk()
-    // screenshotMsg.innerHTML = filePath;
-    // console.log(filePath)
-})
+
+// screenshot.addEventListener('click', async () => {
+//   var filePath = await window.electrona.mykk()
+//   // screenshotMsg.innerHTML = filePath;
+//   // console.log(filePath)
+// })
 
 
