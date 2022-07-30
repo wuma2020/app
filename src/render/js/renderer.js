0ab0b9c2bbb36843a7f512cdc27ff06ffafdcaf0
makaikai <makaikai@accesscorporate.com.cn>
2022-07-30 11:05:34 +0800

    tt

--- src/render/js/renderer.js
+++ src/render/js/renderer.js
@@ -5,23 +5,23 @@ const links = document.querySelectorAll('link[rel="import"]')
 // Import and add each page to the DOM
 Array.prototype.forEach.call(links, (link) => {
 
-    console.log(link)
-    let template = link.import.querySelector('.template')
+  console.log(link)
+//   let template = link.import.querySelector('.template')
 
-    console.log(template)
+//   console.log(template)
 
-    let clone = document.importNode(template.content, true)
+//   let clone = document.importNode(template.content, true)
 
-    if (link.href.match('head.html')) {
-        document.querySelector('head').appendChild(clone)
-    }
+  if (link.href.match('head.html')) {
+    //这个方法可以加载一下
+    $('#head').load('../static/head.html')
+    // document.querySelector('head').appendChild(clone)
+  }
 
-    if (link.href.match('body.html')) {
-        document.querySelector('body').appendChild(clone)
-    }
+//   if (link.href.match('body.html')) {
+//     document.querySelector('body').appendChild(clone)
+//   }
 })
-
-
 
 const NOTIFICATION_TITLE = '网络变化通知'
 const NOTIFICATION_BODY = 'Notification from the Renderer process. Click to log to console.'
