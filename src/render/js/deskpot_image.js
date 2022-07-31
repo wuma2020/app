60d3a0cc36a4aa2e636abd5662b4d43cf14587cb
makaikai <makaikai@accesscorporate.com.cn>
2022-07-31 22:31:37 +0800

    截图

--- src/render/js/deskpot_image.js
+++ src/render/js/deskpot_image.js
@@ -1,11 +1,31 @@
-  //截图功能
-  $("#short-cut-button").click(function (e) { 
-    console.log("截图功能")
-    
-  });
+//截图功能
+$("#short-cut-button").click(function (e) {
+  console.log("截图功能")
+
+  //弹出弹窗
+  desktopWindow()
+
+});
 
 
 
- var desktopCapturer = require('electron').desktopCapturer
+var desktopCapturer = require('electron').desktopCapturer
 
- console.log(desktopCapturer)
+console.log(desktopCapturer)
+
+const electron = require('electron')
+const BrowserWindow = electron.remote.BrowserWindow
+
+
+var desktopWindow = () => {
+  const currWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  console.log(currWindow)
+  currWindow.loadFile('./src/static/cut_window.html')
+}
+
+//生效了 这里可以做绘制截图的页面
+// desktopWindow()
--- src/render/js/preload.js
+++ src/render/js/preload.js
@@ -6,54 +6,6 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-const { ipcRenderer ,contextBridge} = require('electron')
-
-ipcRenderer.on('SET_SOURCE', async (event, sourceId) => {
-  try {
-    console.log("ipcRenderer on ")
-    const stream = await navigator.mediaDevices.getUserMedia({
-      audio: false,
-      video: {
-        mandatory: {
-          chromeMediaSource: 'desktop',
-          chromeMediaSourceId: sourceId,
-          minWidth: 1280,
-          maxWidth: 1280,
-          minHeight: 720,
-          maxHeight: 720
-        }
-      }
-    })
-    handleStream(stream)
-  } catch (e) {
-    handleError(e)
-  }
-})
-
-ipcRenderer.on('ipcRendererShortcut',(data)=>{
-  console.log("ipcRendererShortcut " + data)
-})
-
-
-function handleStream (stream) {
-  const video = document.querySelector('video')
-  video.srcObject = stream
-  video.onloadedmetadata = (e) => video.play()
-}
-
-function handleError (e) {
-  console.log(e)
-}
-
-
-
-contextBridge.exposeInMainWorld('electrona',
-{
-  mykk: () => {
-    var aa = ipcRenderer.invoke('shortchat',"aa")
-    return aa;
-  }
-})
 
 
 
